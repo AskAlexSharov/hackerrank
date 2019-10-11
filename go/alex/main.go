f2e32fb940186c4c94e0bfeda9751cc8f451456c
alex.sharov <alex.sharov@lazada.com>
2019-10-11 17:52:49 +0700

    ufff... probably it's time to move to recursive algo which is better for immutable data

---
+++ go/alex/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// POST /transaction/?request_guid=..&amount=+1
+
+}
--- rust/binary_search_tree/src/main.rs
+++ rust/binary_search_tree/src/main.rs
@@ -1,9 +1,9 @@
 //#![feature(type_alias_impl_trait)]
 
 use serde::export::fmt::Debug;
-use std::borrow::BorrowMut;
+use std::borrow::{Borrow, BorrowMut};
 
-type Visitor<T> = Box<Fn(Node<T>) -> bool>;
+type Visitor<T> = Box<Fn(&Node<T>) -> bool>;
 
 trait Holdable: Ord + Debug + Default {}
 
@@ -11,9 +11,9 @@ impl<T: Ord + Debug + Default> Holdable for T {}
 
 trait Tree<T: Holdable>: std::fmt::Debug {
     fn insert(&mut self, val: T);
-    fn inorder(&self, visitor: Visitor<T>);
-    fn preorder(&self, visitor: Visitor<T>);
-    fn postorder(&self, visitor: Visitor<T>);
+    fn inorder(&mut self, visitor: Visitor<T>);
+    fn preorder(&mut self, visitor: Visitor<T>);
+    fn postorder(&mut self, visitor: Visitor<T>);
 }
 
 type NodeId = usize;
@@ -67,6 +67,17 @@ struct MorrisTree<T: Holdable> {
     arena: Arena<T>,
 }
 
+fn left_right_most_child<T: Holdable>(node_id: NodeId, arena: &mut Arena<T>) -> Option<NodeId> {
+    let mut child_id = arena.get(node_id).left?;
+    let child = arena.get(child_id);
+
+    while child.right.is_some() && child.right.unwrap() != node_id {
+        child_id = child.right.unwrap();
+    }
+
+    Some(child_id)
+}
+
 impl<T: Holdable> Tree<T> for MorrisTree<T> {
     fn insert(&mut self, val: T) {
         let arena = self.arena.borrow_mut();
@@ -79,6 +90,11 @@ impl<T: Holdable> Tree<T> for MorrisTree<T> {
         let mut node_id = self.root.unwrap();
         loop {
             let node = arena.get(node_id);
+
+            if val == node.data {
+                break;
+            }
+
             if val < node.data {
                 if node.left.is_none() {
                     arena.get_mut(node_id).left = Some(arena.add(val));
@@ -97,13 +113,50 @@ impl<T: Holdable> Tree<T> for MorrisTree<T> {
         }
     }
 
-    fn inorder(&self, visitor: Visitor<T>) {
+    fn inorder(&mut self, visitor: Visitor<T>) {
+        let mut node_id = self.root;
+        let arena = self.arena.borrow_mut();
+
+        while node_id.is_some() {
+            let node = arena.get_mut(node_id.unwrap());
+            if node.left.is_none() {
+                if !visitor(node) {
+                    break;
+                }
+
+                node_id = node.right;
+                continue;
+            }
+
+            // left_right_most_child
+            let mut pre_id = node.left.unwrap();
+            let pre = arena.get_mut(pre_id);
+            while pre.right.is_some() && pre.right.unwrap() != pre_id {
+                pre_id = pre.right.unwrap();
+            }
+
+            let pre_node = arena.get_mut(pre_id);
+
+            if pre_node.right.is_none() || pre_node.right.unwrap() != node_id.unwrap() {
+                pre_node.right = Some(node_id.unwrap());
+                node_id = node.left;
+                continue;
+            }
+
+            pre_node.right.take();
+
+            if !visitor(node) {
+                break;
+            }
+
+            node_id = node.right;
+        }
         panic!("not implemented yet!")
     }
-    fn preorder(&self, visitor: Visitor<T>) {
+    fn preorder(&mut self, visitor: Visitor<T>) {
         panic!("not implemented yet!")
     }
-    fn postorder(&self, visitor: Visitor<T>) {
+    fn postorder(&mut self, visitor: Visitor<T>) {
         panic!("not implemented yet!")
     }
 }
@@ -131,6 +184,10 @@ fn main() {
     let data = prepare_data_for_tree(10);
 
     build_binary_search_tree(&mut tree, data);
+    tree.inorder(Box::new(|node| {
+        dbg!(node);
+        true
+    }));
 
-    dbg!(tree);
+    //    dbg!(tree);
 }
